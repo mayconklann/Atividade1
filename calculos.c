2f486cf1c562bbe531fa408ca65a2ae8a5d28de4
Maycon Klann <maycon.klann@gmail.com>
2020-03-19 18:12:52 -0300

    Adição de interface

--- calculos.c
+++ calculos.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Este programa é um exemplo básico para as aplicações de controle de versionamento GIT
 
@@ -7,8 +8,16 @@ int main(){
 	int a = 3;
 	int b = 4;
 
+	printf("Digite um número inteiro: ");
+	scanf("%d", &a);
+	printf("Digite outro número inteiro: ");
+	scanf("%d", &b);
+
 	int resposta_a = soma(a,b);
 	int resposta_b = mult(a,b);
+
+	printf("A soma e': %d\n",resposta_a);
+	printf("A multiplicacao e': %d\n",resposta_b);
 
 	return 0;
 }
